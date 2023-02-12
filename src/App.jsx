640f2aed44b378117a2e15370c9efddc716c1c93
Guilherme Marins <106755788+marinsgui@users.noreply.github.com>
2023-02-12 11:55:13 -0300

    formulário inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log(email, password)
+  }
   
 
   return (
     <div className="App">
-      <h1>Hello, world!</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email: 
+          <input 
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)} 
+          />
+        </label>
+        <label>
+          Senha: 
+          <input 
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)} 
+          />
+        </label>
+        <button type='submit'>Cadastrar</button>
+      </form>
     </div>
   )
 }
